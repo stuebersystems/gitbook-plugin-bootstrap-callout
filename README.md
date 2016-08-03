09309dc277858b134c810ac04dcca07598d6cf3a
Frank Stüber <frank@stueber.de>
2016-08-03 17:50:51 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,56 @@
+# Gitbook Callout Plugin
+
+This plugin creates [Bootstrap](https://getbootstrap.com/) like callouts for your GitBook. All three GitBook themes (White, Sepia, Night) are supported. This project took heavy inspiration from https://github.com/gubler/gitbook-plugin-callouts.
+
+## Usage
+
+### Installation
+
+Add the plugin to your `book.json`:
+
+```
+{
+	"plugins" : [ "bootstrap-callout" ]
+}		
+```
+
+### Create callouts
+
+Callouts are an extension of markdown blockquotes. All callouts must start with a h4 header, the callout type, and a title. The format for this is:
+
+For Markdown:
+
+```
+> #### type::Title
+>
+> Content
+```
+
+Supported callouts are:
+
+- `> #### primary::Title`
+- `> #### success::Title`
+- `> #### danger::Title`
+- `> #### warning::Title`
+- `> #### info::Title`
+
+If you leave out the type a default callout is used
+
+```
+> #### Title
+>
+> Content
+```
+
+Callouts are case-insensitive.
+
+## Screenshots
+
+The callouts in action (White theme):
+
+![Screenshots](screenshots.png)
+
+## Changelog
+
+* 0.1.0 Releases:
+  * 0.1.0 First release
---
+++ index.js
@@ -0,0 +1,72 @@
+var cheerio = require( "cheerio" )
+   
+var convertBlockquotes = function (page)
+{
+	var $ = cheerio.load( page.content );
+	
+	// For each blockquote...
+	$( "blockquote" ).each(function () {
+		
+		// Find first h4 header
+		var h4Header = $(this).find('h4:first-child');
+		
+		// Cancel if no header found
+		if( !h4Header || h4Header.length == 0) 
+		{
+			return;
+		}
+		
+		// Split header text
+		var headerParts = $(h4Header).text().split('::', 2);
+
+		// Set style and title
+		var style = headerParts[0].toLowerCase() ? headerParts[0].toLowerCase() : "default";
+		var title = (headerParts[1] === "") ? headerParts[0] : headerParts[1];
+
+		// Store all segments as array
+		var children = $(this).children().toArray();
+		
+		// Remove the first segment
+		children.shift();
+
+		// Create callout header
+		var calloutHeader = $('<h4>').append(title);
+
+		// Create callout bodyy
+		var calloutBody = $('<div>').append(children);
+
+		// Create callout
+		var callout = $('<div>')
+			.addClass('callout callout-' + style)
+			.append(calloutHeader)
+			.append(calloutBody);
+
+		// Insert new callout to DOM
+		$(this).before(callout);
+
+		// Remove old blockquote
+		$(this).remove();
+
+		// Replace page content 
+		page.content = $.html();
+		
+	});
+
+	return page;
+};
+
+module.exports = {
+	book: {
+			assets: './assets',
+			css: [
+				'plugin.css'
+			]
+    },
+	hooks: {
+			"page": function(page) 
+			{
+				page = convertBlockquotes(page);
+				return page;
+	     	}
+    }
+};
